33765cdb0b8c50ed539ca172c74ec53d71569cd0
Amisha Tripathy <tripathyamisha3@gmail.com>
2026-06-26 13:45:46 +0530

    Added Streamlit Secrets support

--- AI/modules/gemini_explainer.py
+++ AI/modules/gemini_explainer.py
@@ -1,5 +1,7 @@
 # Gemini explainer module
 
+from turtle import st
+
 import google.generativeai as genai
 
 import os
@@ -9,14 +11,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# First, try to get the key from .env
+api_key = os.getenv("GOOGLE_API_KEY")
 
-genai.configure(
+# If not found, get it from Streamlit Secrets
+if not api_key:
 
-api_key=os.getenv("GOOGLE_API_KEY")
+    api_key = st.secrets["GOOGLE_API_KEY"]
 
-)
+# Configure Gemini
 
-
+genai.configure(api_key=api_key)
 
 def get_gemini_response(text):
 
